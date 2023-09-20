a7997ccfc0703a8e3b3db7ccbe8134441018858e
Foram22 <foramjpatel5778@gmail.com>
2023-09-20 16:30:56 -0400

    created two activities for receiver and creator to show their home page, notification and profile page.

---
+++ app/src/main/java/com/foram/postalmailreceiver/Activity/CreatorHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.foram.postalmailreceiver.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foram.postalmailreceiver.R
+
+class CreatorHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creator_home)
+    }
+}
